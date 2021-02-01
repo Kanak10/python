c698edb169b363ac758fe285aadf3f114b91a20c
Kanak <Kanakgupta814@gmail.com>
2021-02-01 13:57:07 +0530

    List complete

--- list.py
+++ list.py
@@ -9,6 +9,9 @@ Each element in the list has its definite place in the list, which allows duplic
 with each element having its own distinct place and credibility.
 Note- Lists are a useful tool for preserving a sequence of data and further iterating over it.
 """
+from typing import get_origin
+
+
 grocery = ["Harpic" , "Vim bar" , "Dettol" , "Rice" , "Noodles" , "Sugar"]
 
 #print list
@@ -20,17 +23,71 @@ print(grocery[4]+"\n")
 #print using for loop
 for i in grocery:
     print(i)
+print("\n")
 
 n = [4,8,6,2,1,8,15,45,45,12,48,54,]
 
+#The index() method returns the index of the specified element in the list.
+print(grocery.index("Sugar"))
 
+#The append() method adds an item to the end of the list.
+grocery.append("Vegetable oil")
+print(grocery)
 
+#The extend() method adds all the elements of an iterable (list, tuple, string etc.) to the end of the list.
+g = ["Bodylotion", "Hair oil"]
+grocery.extend(g)
+print(grocery)
 
+#The list insert() method inserts an element to the list at the specified index.
+grocery.insert(3,"Shampoo")
+print(grocery)
 
+#The remove() method removes the first matching element (which is passed as an argument) from the list.
+grocery.remove("Shampoo")
+print(grocery)
 
+#The count() method returns the number of times the specified element appears in the list.
+print(grocery.count("Rice"))
 
+#The pop() method removes the item at the given index from the list and returns the removed item.
+print(grocery.pop(5))
 
+#The reverse() method reverses the elements of the list.
+grocery.reverse()
+print(grocery)
 
+#The sort() method sorts the elements of a given list in a specific ascending or descending order.
+#syntax = list.sort(key=..., reverse=...), sorted(list, key=..., reverse=...)
+print(sorted(grocery))
 
+employees = [
+    {'Name': 'Alan Turing', 'age': 25, 'salary': 10000},
+    {'Name': 'Sharon Lin', 'age': 30, 'salary': 8000},
+    {'Name': 'John Hopkins', 'age': 18, 'salary': 1000},
+    {'Name': 'Mikhail Tal', 'age': 40, 'salary': 15000},
+]
 
+def get_name(employee):
+    return employee.get('Name')
 
+def get_age(employee):
+    return employee.get('age')
+
+def get_salary(employee):
+    return employee.get('salary')
+
+employees.sort(key=get_name)
+print(employees, end='\n\n')
+
+employees.sort(key=get_age)
+print(employees, end='\n\n')
+
+employees.sort(key=get_salary, reverse=True)
+print(employees, end='\n\n')
+
+#The copy() method returns a shallow copy of the list.
+print(grocery.copy())
+
+#The clear() method removes all items from the list.
+print(grocery.clear())
